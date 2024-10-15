4122561caf174f62284ec4b487f1b398fee49520
hieutdle <hieu.tdle@gmail.com>
2024-10-15 17:30:15 +0200

    improve test files

--- monkey/test/basic.cc
+++ monkey/test/basic.cc
@@ -4,6 +4,9 @@
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
 
+  // Set GoogleTest to stop on first failure
+  GTEST_FLAG_SET(break_on_failure, true);
+
   // Run all tests
   return RUN_ALL_TESTS();
 }
